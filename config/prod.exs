4ca1da1344c985550ee45faf62c57928c0055bbe
OCannings <oli@live.co.uk>
2016-05-13 22:19:47 +0100

    chore(config): set secret_key_base

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,8 @@ use Mix.Config
 # which you typically run after static files are built.
 config :juicebox, Juicebox.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [host: "example.com", port: 80]
+#  cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -55,7 +55,12 @@ config :logger, level: :info
 #
 #     config :juicebox, Juicebox.Endpoint, server: true
 #
+# Configure secret_key_base for cookie-based session storage
+config :juicebox, Juicebox.Endpoint,
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
+# Configure your database
+config :juicebox, Juicebox.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  size: 20 # The amount of database connections in the pool
