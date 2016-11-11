d98c05c504cd08b849bea9ae124088bdd6a685a9
OCannings <oli@live.co.uk>
2016-11-11 10:17:06 +0000

    fix(Stream): crash with string SILENCE_TIME config
    Convert SILENCE_TIME to integer to prevent arithmetic error in Stream
    process.

--- apps/juicebox_stream/config/prod.exs
+++ apps/juicebox_stream/config/prod.exs
@@ -10,6 +10,10 @@ config :juicebox_stream, JuiceboxStream.Repo,
   size: 20 # The amount of database connections in the pool
 
 config :juicebox_stream, :youtube_api, JuiceboxStream.Youtube.API
+config :juicebox_stream,
+  :silence_time,
+  String.to_integer(System.get_env("SILENCE_TIME")) # silence between tracks
+
 
 config :juicebox_stream, JuiceboxStream.Youtube,
   api_key: System.get_env("YOUTUBE_API_KEY"),
