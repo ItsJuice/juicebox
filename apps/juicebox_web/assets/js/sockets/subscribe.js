1f4fe8413984ee7cfa96c5e46f84dcaa1b527aac
OCannings <oli@live.co.uk>
2017-04-04 21:48:55 +0100

    fix(Sockets): recursive dispatch on stream change
    Fixing the tests revealed that an action was being dispatched
    repeatedly when the streamId changed.
    
    An action was being dispatched inside the subscribe callback before the
    check which was designed to prevent recursion. (`newStream ===
    oldStream`)
    
    `subscribe.js` has been updated to ensure the action is only dispatched
    once when the streamId changes.
    
    Tests are now passing.

--- apps/juicebox_web/assets/js/sockets/subscribe.js
+++ apps/juicebox_web/assets/js/sockets/subscribe.js
@@ -1,20 +1,27 @@
 import { matchPath } from 'react-router';
 
-function checkStreamChange(action, store, oldStream) {
+function streamIdFromPath() {
   const path = '/stream/:streamId';
   const match = matchPath(document.location.pathname, { path });
-  const newStream = match && match.params.streamId;
+  const streamId = match && match.params.streamId;
 
-  if (newStream === oldStream) { return oldStream; }
-
-  store.dispatch(action(newStream));
-  return newStream;
+  return streamId;
 }
 
-export default ({ action, store }) => {
-  let oldStream = checkStreamChange(action, store, oldStream);
+export default ({ action, store, getStreamId = streamIdFromPath }) => {
+  let currentStream = getStreamId();
+
+  const dispatchStreamChange = (streamId) =>
+    store.dispatch(action(streamId));
+
+  dispatchStreamChange(currentStream);
 
   store.subscribe(() => {
-    oldStream = checkStreamChange(action, store, oldStream);
+    const newStream = getStreamId();
+
+    if (newStream !== currentStream) {
+      currentStream = newStream;
+      dispatchStreamChange(currentStream);
+    }
   });
 }
